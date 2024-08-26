d3ee3fe6f1e0613520c00b021cea8e2ffe1f31f1
barbarazamperete <barbarazamperete@mprr.mp.br>
2024-08-26 16:54:46 -0400

    Novas entradas e novos graficos

--- seminario_selection_sort/geradordenumeros.c
+++ seminario_selection_sort/geradordenumeros.c
@@ -4,20 +4,23 @@
 #include <sys/stat.h>
 
 #ifdef _WIN32
-    #include <direct.h>  // Necessário para o Windows
-    #define MKDIR(a) _mkdir(a)
+#include <direct.h> // Necessário para o Windows
+#define MKDIR(a) _mkdir(a)
 #else
-    #include <unistd.h>  // Necessário para sistemas Unix-like
-    #define MKDIR(a) mkdir(a, 0700)
+#include <unistd.h> // Necessário para sistemas Unix-like
+#define MKDIR(a) mkdir(a, 0700)
 #endif
 
-void gerar_numeros(int *lista, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
+void gerar_numeros(int *lista, int tamanho)
+{
+    for (int i = 0; i < tamanho; i++)
+    {
         lista[i] = rand() % 10001;
     }
 }
 
-int main() {
+int main()
+{
     const char *pasta = "numeros_aleatorios";
     struct stat st = {0};
 
@@ -25,17 +28,21 @@ int main() {
     srand(time(NULL));
 
     // Cria o diretório se não existir
-     // Cria o diretório se não existir
-    if (stat(pasta, &st) == -1) {
-        if (MKDIR(pasta) != 0) {
+    // Cria o diretório se não existir
+    if (stat(pasta, &st) == -1)
+    {
+        if (MKDIR(pasta) != 0)
+        {
             perror("Não foi possível criar o diretório");
             return 1;
         }
     }
 
-    for (int n = 100000; n <= 200000; n += 1000) {
+    for (int n = 1000; n <= 100000; n += (n == 1000 ? 4000 : 5000))
+    {
         int *lista = (int *)malloc(n * sizeof(int));
-        if (lista == NULL) {
+        if (lista == NULL)
+        {
             perror("Não foi possível alocar memória");
             return 1;
         }
@@ -47,13 +54,15 @@ int main() {
         snprintf(nome_arquivo, sizeof(nome_arquivo), "%s/numeros_%d.txt", pasta, n);
 
         FILE *arquivo = fopen(nome_arquivo, "w");
-        if (arquivo == NULL) {
+        if (arquivo == NULL)
+        {
             perror("Não foi possível abrir o arquivo");
             free(lista);
             return 1;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             fprintf(arquivo, "%d\n", lista[i]);
         }
 
